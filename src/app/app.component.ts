1179c962f1b7bd5fc2cc298cd265c1e6a2459d49
Muriel Ighmouracene <muriel.ighmouracene@gmail.com>
2024-10-02 12:04:23 +0200

    Login form install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component'; // Import du composant ArticleComponent
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleComponent],  // Ajout d'ArticleComponent dans les imports
+  imports: [RouterOutlet, ArticleComponent, LoginFormComponent],  // Ajout d'ArticleComponent dans les imports
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
